e47446769a7cb3207f2263a0f39daf4741766736
Achang009 <146834990+Achang009@users.noreply.github.com>
2026-01-14 23:59:18 +0800

    Update FSMupdown_counter.v

--- FSMupdown_counter.v
+++ FSMupdown_counter.v
@@ -12,31 +12,33 @@ entity FSMupdown_counter is
 end FSMupdown_counter;
 
 architecture Behavioral of FSMupdown_counter is
- 
-    signal state   : STD_LOGIC := '0'; 
-    signal cntup   : unsigned(3 downto 0) := (others => '0');
-    signal cntdown : unsigned(3 downto 0) := (others => '0');
-    constant c_max_count : integer := 50000000; 
-    signal r_div_cnt     : integer range 0 to c_max_count := 0;
-    signal w_tick        : std_logic := '0';
+
+    constant max         : unsigned(3 downto 0) := "1111";
+    constant min         : unsigned(3 downto 0) := "0000";
+    constant C_MAX_COUNT : integer := 50000000; 
+
+    signal state     : STD_LOGIC := '0'; 
+    signal cntup     : unsigned(3 downto 0) := min;
+    signal cntdown   : unsigned(3 downto 0) := min;
+    signal r_div_cnt : integer range 0 to C_MAX_COUNT := 0;
+    signal w_tick    : std_logic := '0';
 
 begin
-
-    FREQ_DIVIDER: process(i_clk, i_reset)
+    freq: process(i_clk, i_reset)
     begin
         if i_reset = '1' then
             r_div_cnt <= 0;
             w_tick    <= '0';
         elsif rising_edge(i_clk) then
             w_tick <= '0'; 
-            if r_div_cnt = c_max_count then
+            if r_div_cnt = C_MAX_COUNT then
                 r_div_cnt <= 0;
                 w_tick    <= '1';
             else
                 r_div_cnt <= r_div_cnt + 1;
             end if;
         end if;
-    end process FREQ_DIVIDER;
+    end process freq;
 
     FSM: process(i_clk, i_reset)
     begin
@@ -46,15 +48,13 @@ begin
             if w_tick = '1' then
                 case state is
                     when '0' =>
-                        if cntup >= 15 then
+                        if cntup >= max then
                             state <= '1';
                         end if;
-
                     when '1' =>
-                        if cntdown <= 0 then
+                        if cntdown <= min then
                             state <= '0';
-                        end if;
-                    
+                        end if; 
                     when others =>
                         state <= '0';
                 end case;
@@ -65,17 +65,17 @@ begin
     up_counter: process(i_clk, i_reset)
     begin
         if i_reset = '1' then
-            cntup <= to_unsigned(0, 4);
+            cntup <= min;
         elsif rising_edge(i_clk) then
             if w_tick = '1' then
                 if state = '0' then
-                    if cntup >= 15 then
-                        cntup <= to_unsigned(15, 4);
+                    if cntup >= max then
+                        cntup <= max;
                     else
                         cntup <= cntup + 1;
                     end if;
                 else
-                    cntup <= to_unsigned(0, 4);
+                    cntup <= min;
                 end if;
             end if;
         end if;
@@ -84,24 +84,21 @@ begin
     down_counter: process(i_clk, i_reset)
     begin
         if i_reset = '1' then
-            cntdown <= to_unsigned(15, 4);
+            cntdown <= max;
         elsif rising_edge(i_clk) then
             if w_tick = '1' then
                 if state = '1' then
-                    if cntdown <= 0 then
-                        cntdown <= to_unsigned(0, 4);
+                    if cntdown <= min then
+                        cntdown <= min;
                     else
                         cntdown <= cntdown - 1;
                     end if;
                 else
-                    cntdown <= to_unsigned(15, 4);
+                    cntdown <= max;
                 end if;
             end if;
         end if;
     end process down_counter;
-            o_countup   <= std_logic_vector(cntup);
-            o_countdown <= std_logic_vector(cntdown);
+    o_countup   <= std_logic_vector(cntup);
+    o_countdown <= std_logic_vector(cntdown);
 end Behavioral;
-
-
-
